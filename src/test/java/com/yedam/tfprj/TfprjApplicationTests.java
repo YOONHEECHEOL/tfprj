34dce734f8262296c10d70a9c6215da0f139bb0a
YOONHEECHEOL <luk2903201@naver.com>
2022-05-09 09:52:34 +0900

    Initial commit

---
+++ src/test/java/com/yedam/tfprj/TfprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yedam.tfprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfprjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
