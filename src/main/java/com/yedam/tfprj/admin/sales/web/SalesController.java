eedce0551ad6d379c19aab2ea4c1c9b44ea2532b
audghks1996 <audghks1996@naver.com>
2022-05-12 16:01:42 +0900

    명환 Controller 생성

---
+++ src/main/java/com/yedam/tfprj/admin/sales/web/SalesController.java
@@ -0,0 +1,12 @@
+package com.yedam.tfprj.admin.sales.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SalesController {
+    @RequestMapping("/adm/salesManage")
+    public String admSalesManage(){
+        return "admin/sales/sales_manage";
+    }
+}
