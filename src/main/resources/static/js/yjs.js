c0ca8921c278f6135d66479ad4a28d98b784987b
seonghun <tmal82533@gmail.com>
2022-05-17 11:25:19 +0900

    0517 공지사항 조회 에디터 적용중

--- src/main/resources/static/js/yjs.js
+++ src/main/resources/static/js/yjs.js
@@ -9,12 +9,3 @@
 //     console.log(event.target)
 //   })
 // })
-
-let cliSliderMenu = document.querySelector('#cli-slider-menu')
-let cliSliderNav = document.querySelector('#cli-slider-nav')
-let cliSliderNavBg = document.querySelector('#cli-slider-nav-bg')
-
-cliSliderMenu.addEventListener('click', () => {
-  cliSliderNav.classList.toggle('active')
-  cliSliderNavBg.classList.toggle('active')
-})
