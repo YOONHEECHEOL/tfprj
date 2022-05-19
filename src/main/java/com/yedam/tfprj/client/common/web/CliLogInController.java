0aa9433d9c542513e837b4c4d7f7f7e561b62fd2
seonghun <tmal82533@gmail.com>
2022-05-19 09:36:29 +0900

    0518 공지사항 게시판 수정(수정필요) 삭제

--- src/main/java/com/yedam/tfprj/client/common/web/CliLogInController.java
+++ src/main/java/com/yedam/tfprj/client/common/web/CliLogInController.java
@@ -6,10 +6,12 @@ import com.yedam.tfprj.client.member.service.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -68,11 +70,8 @@ public class CliLogInController {
     // 회원가입 처리
     @PostMapping("/cli/insertMember")
     public String insert(MemberVO vo){
-        System.out.println("vo = " + vo.getMemberId());
-        System.out.println("vo = " + vo.getPassword());
-        System.out.println("vo = " + vo.getTel());
 
-        memberServiceImpl.insertMember(vo);
+       memberServiceImpl.insertMember(vo);
         return "redirect:/cli/loginview";
     }
 
