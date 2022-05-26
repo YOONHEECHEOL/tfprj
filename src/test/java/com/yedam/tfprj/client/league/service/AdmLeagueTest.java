384860843d0848e2fc3724c185e508f2ec5af200
YOONHEECHEOL <luk2903201@naver.com>
2022-05-26 17:58:29 +0900

    220526 관리자 league detail 화면 수정 중

---
+++ src/test/java/com/yedam/tfprj/client/league/service/AdmLeagueTest.java
@@ -0,0 +1,15 @@
+package com.yedam.tfprj.client.league.service;
+
+import com.yedam.tfprj.admin.league.service.AdmLeagueServiceImpl;
+import org.junit.jupiter.api.Test;
+
+public class AdmLeagueTest {
+
+    AdmLeagueServiceImpl admLeagueService = new AdmLeagueServiceImpl();
+
+    @Test
+    public void getLeagueApplyTeamIsApprove() {
+//        admLeagueService.getLeagueApplyTeam
+    }
+
+}
