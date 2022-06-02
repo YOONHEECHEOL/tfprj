6785eb3dde97f0d6d26ec515647907e09d35658f
seonghun <tmal82533@gmail.com>
2022-06-02 17:46:12 +0900

    0602 끝

--- src/main/java/com/yedam/tfprj/admin/community/web/AdmFmController.java
+++ src/main/java/com/yedam/tfprj/admin/community/web/AdmFmController.java
@@ -7,9 +7,9 @@ import com.yedam.tfprj.admin.community.service.fm.AdmFmVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdmFmController {
@@ -64,7 +64,7 @@ public class AdmFmController {
     @RequestMapping("/adm/fmWrite")
     public String AdmFmWrite(Model model, AdmFmVO vo) {
         model.addAttribute("fm", vo);
-        return "/admin/community/fm/fmWrite";
+        return "admin/community/fm/fm_write";
     }
 
     @RequestMapping("/adm/fmInsert")
@@ -84,10 +84,4 @@ public class AdmFmController {
     }
 
 
-    @RequestMapping("/adm/fmSearch")
-    @ResponseBody
-    public List<AdmFmVO> AdmFmSearch(AdmFmVO vo) {
-
-        return service.AdmFmSearch(vo);
-    }
 }
