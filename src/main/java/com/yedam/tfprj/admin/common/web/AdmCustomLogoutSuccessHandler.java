c9468834aa306457c4d1a5aba23543fc8e52ede8
96ssg <sinsa2709@gmail.com>
2022-06-08 12:53:04 +0900

    list 목록 조회 검색 기능 수정 기록조회 테이블 생성

--- src/main/java/com/yedam/tfprj/admin/common/web/AdmCustomLogoutSuccessHandler.java
+++ src/main/java/com/yedam/tfprj/admin/common/web/AdmCustomLogoutSuccessHandler.java
@@ -2,7 +2,6 @@ package com.yedam.tfprj.admin.common.web;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.yaml.snakeyaml.util.UriEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +11,6 @@ import java.io.IOException;
 public class AdmCustomLogoutSuccessHandler implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        System.out.println("관리자로그아웃 후 ===========================" + request.getRequestURI().contains("/cli"));
-        if(request.getRequestURI().contains("/adm")){
-            response.sendRedirect("/adm/loginview");
-        }else{
-            response.sendRedirect("/cli/loginview");
-        }
-
+        response.sendRedirect("/adm/loginview");
     }
 }
--- src/main/java/com/yedam/tfprj/client/common/web/CustomLogoutSuccessHandler.java
+++ src/main/java/com/yedam/tfprj/client/common/web/CustomLogoutSuccessHandler.java
@@ -13,12 +13,6 @@ import java.io.IOException;
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        System.out.println("사용자로그아웃 후 ===========================" + request.getHeader("Referer"));
-        if(request.getHeader("Referer").startsWith("/cli")){
-            response.sendRedirect("/cli/home?message="+ UriEncoder.encode("로그아웃 되었습니다."));
-        }else{
-            response.sendRedirect("/adm/loginview");
-        }
-
+        response.sendRedirect("/cli/home?message="+ UriEncoder.encode("로그아웃 되었습니다."));
     }
 }
